e8dac5b3eba291c084b66072e474fd81d2dbe76a
Fatheed7 <69724750+Fatheed7@users.noreply.github.com>
2022-08-17 17:24:37 +0100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# main.py
+
+from fastapi import FastAPI
+import json
+
+app = FastAPI()
+
+json_file = json.load(open('conversion.json'))
+
+@app.get("/convert/")
+def hello(type: str, from_type: str, to_type: str, value: float):
+  x = json_file[type][0]
+  if from_type in x.__str__():
+    return {
+      'from_type': from_type,
+      'to_type': to_type,
+      'value': value,
+      'result': value * x[from_type][to_type]
+    }
+  else:
+    return {
+      'from_type': from_type,
+      'to_type': to_type,
+      'value': value,
+      'result': 'Invalid input'
+    }
